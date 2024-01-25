749db7926ea5f488acf3ddc364dfe3b5feaa8fce
AishwariyaD <aishwariyad26@gmail.com>
2024-01-25 15:54:33 +0530

    TWO

---
+++ src/pages/Service.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../css/Service.css";
+export default function Service() {
+
+  return (
+    <div>
+        <div className='service'>
+            <div className='service-item'>A</div>
+            <div className='service-item'>I</div>
+            <div className='service-item'>S</div>
+            <div className='service-item'>H</div>
+            <div className='service-item'>U</div>
+            <div className='service-item'>D</div>
+        </div>
+    </div>
+  )
+}
