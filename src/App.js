5fde2063cf7ef8d7cbfe4c467391e56b812b3282
AishwariyaD <aishwariyad26@gmail.com>
2024-01-26 15:30:06 +0530

    six

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import {BrowserRouter , Routes , Route } from "react-router-dom"
-import Layout from "./Layout"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Service from "./pages/Service"
-export default function App() {
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="contact" element={<Contact/>}/>
-        <Route path="service" element={<Service/>}/>
+// import {BrowserRouter , Routes , Route } from "react-router-dom"
+// import Layout from "./Layout"
+// import Home from "./pages/Home"
+// import About from "./pages/About"
+// import Contact from "./pages/Contact"
+// import Service from "./pages/Service"
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Layout/>}>
+//         <Route index element={<Home/>}/>
+//         <Route path="about" element={<About/>}/>
+//         <Route path="contact" element={<Contact/>}/>
+//         <Route path="service" element={<Service/>}/>
+//         <Route path="effect"element={<Effect/>}/>
 
-     </Route>
-    </Routes>
-    </BrowserRouter>
-  )
-}
+//      </Route>
+//     </Routes>
+//     </BrowserRouter>
+//   )
+// }
 
 // import React from 'react'
-// import Layout from './Layout'
+// import Counter from "./usestate/Counter"
+// import Reducer from './usestate/Reducer'
 // export default function App() {
 //   return (
 //     <div>
-//       <Layout/>
+//       <Reducer/>
+//       <Counter/>
 //     </div>
 //   )
 // }
+
+import React from 'react'
+import Effect from './usestate/Effect'
+import Reducer from './usestate/Reducer'
+import Counter from './usestate/Counter'
+
+export default function App() {
+  return (
+    <div>
+      <Counter/>
+      <Reducer/>
+      <Effect/>
+    </div>
+  )
+}
